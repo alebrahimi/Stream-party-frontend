eac99a546c2463bb557b92e82c9e62a8dbc92bd9
Maytham <37290793+alebrahimi@users.noreply.github.com>
2024-01-13 13:28:33 +0300

    feat: signup logic

--- src/app/signUp/page.js
+++ src/app/signUp/page.js
@@ -3,12 +3,18 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Image from 'next/image';
+import {useUserStore} from '../../store/useUser'
+import { useRouter } from 'next/navigation'
 
-export default function SignUp() {
+export default SignUp = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [checkPassword, setCheckPassword] = useState('');
   const [errMsg, setErrMsg] = useState('');
+
+  const setUser = useUserStore((state) => state.setUser);
+  const router = useRouter()
 
   useEffect(() => {
     if (password !== checkPassword) {
@@ -16,9 +22,9 @@ export default function SignUp() {
     } else {
       setErrMsg('');
     }
-  }, [checkPassword]);
+  }, [checkPassword, password]);
 
-  const submiting = async (e) => {
+  const submitSignUp = async (e) => {
     e.preventDefault();
 
     if ( checkPassword === '' || password === '' || password !== checkPassword) {
@@ -27,20 +33,22 @@ export default function SignUp() {
     }
 
     try {
-      const response = await fetch('https://your-api-endpoint.com/register', {
+      const response = await fetch(`http://localhost:3000/auth/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+          name,
           email,
           password,
         }),
       });
   
       if (response.ok) {
-        localStorage.setItem('jwt', response.jwt)
-        console.log('Account created successfully!');
+         const userData = await response.json();
+         setUser(userData)
+         router.push("/")
       } else {
         console.error('Account creation failed.');
         const data = await response.json();
@@ -51,6 +59,7 @@ export default function SignUp() {
       setErrMsg('An error occurred. Please try again.');
     }
 
+    setName('')
     setEmail('');
     setPassword('');
     setCheckPassword('');
@@ -75,6 +84,24 @@ export default function SignUp() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
+          <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-ellipsis">
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  autoComplete="name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-ellipsis">
                 Email address
@@ -137,7 +164,7 @@ export default function SignUp() {
             <div>
               <button
                 type="submit"
-                onClick={submiting}
+                onClick={submitSignUp}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign up
