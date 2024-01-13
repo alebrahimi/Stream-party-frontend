d4ec810b0411305b35c3bcbf6b918a9ec8e68d29
Amna-Qassim <amnakassim6@gmail.com>
2024-01-13 15:54:29 +0300

    added profile menu and changed routes of links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Link from "next/link"
+import ProfileMenu from "./profileMenu";
 function Navbar() {
     const token = true
     return (
         <header className="flex justify-between items-center">
             <h1 className="text-2xl">Stream Parties</h1>
             {
-                !token ?
-                    <h1>hi</h1> :
+                token ?
+                    <ProfileMenu /> :
                     <ul className="list-none flex justify-between items-center gap-3">
-                        <Link href="#" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" ><li>Login</li></Link>
-                        <Link href="#" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" ><li>Register</li></Link>
+                        <Link href="signIn" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" ><li>SignIn</li></Link>
+                        <Link href="signUp" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" ><li>SignUp</li></Link>
                     </ul>
             }
         </header>
